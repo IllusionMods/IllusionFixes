a8091d8455b386c09d081646f62ee7dded50099f
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-06-14 16:27:09 -0500

    Added NullChecks

---
+++ Common/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Common
+{
+    internal static class Extensions
+    {
+        public static T[] RemoveNulls<T>(this T[] array)
+        {
+            var list = array.ToList();
+
+            for (int i = 0; i < list.Count;)
+                if (list[i] == null)
+                    list.RemoveAt(i);
+                else
+                    i++;
+
+            return list.ToArray();
+        }
+    }
+}
---
+++ KK_Fix_NullChecks/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KK_Fix_NullChecks")]
+[assembly: AssemblyProduct("KK_Fix_NullChecks")]
+[assembly: AssemblyDescription("Null Checks for Koikatsu")]
+[assembly: Guid("40967983-df5c-411a-bfa4-96c6cfebc63e")]
