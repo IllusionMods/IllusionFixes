cc11ca4880f960c12325237984eb192a963f8bc8
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-06-17 18:32:42 -0500

    Added MakerOptimization

---
+++ KK_Fix_MakerOptimization/CursorManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace KK_Fix_MakerOptimization
+{
+    public class CursorManager : MonoBehaviour
+    {
+        private bool mouseButtonDown0 = false;
+        private bool mouseButtonDown1 = false;
+        private WinCursor.Point lockPos;
+        private bool cursorLocked = false;
+
+        private void Update()
+        {
+            if (MakerOptimization.ManageCursor.Value && Application.isFocused)
+            {
+                if (!cursorLocked)
+                {
+                    if (GUIUtility.hotControl == 0 && !EventSystem.current.IsPointerOverGameObject())
+                    {
+                        bool mouseDown0 = Input.GetMouseButtonDown(0);
+                        bool mouseDown1 = Input.GetMouseButtonDown(1);
+
+                        if (mouseDown0 || mouseDown1)
+                        {
+                            if (mouseDown0) mouseButtonDown0 = true;
+                            if (mouseDown1) mouseButtonDown1 = true;
+
+                            Cursor.visible = false;
+                            Cursor.lockState = CursorLockMode.Confined;
+                            cursorLocked = true;
+                            WinCursor.GetCursorPos(out lockPos);
+                        }
+                    }
+                }
+
+                if (cursorLocked)
+                {
+                    bool mouseUp0 = Input.GetMouseButtonUp(0);
+                    bool mouseUp1 = Input.GetMouseButtonUp(1);
+
+                    if ((mouseButtonDown0 || mouseButtonDown1) && (mouseUp0 || mouseUp1))
+                    {
+                        if (mouseUp0) mouseButtonDown0 = false;
+                        if (mouseUp1) mouseButtonDown1 = false;
+
+                        if (!mouseButtonDown0 && !mouseButtonDown1)
+                        {
+                            Cursor.lockState = CursorLockMode.None;
+                            Cursor.visible = true;
+                            cursorLocked = false;
+                        }
+                    }
+
+                    if (cursorLocked)
+                        WinCursor.SetCursorPos(lockPos.x, lockPos.y);
+                }
+            }
+            else if (cursorLocked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                cursorLocked = false;
+            }
+        }
+
+        private static class WinCursor
+        {
+            [DllImport("user32.dll")]
+            public static extern bool SetCursorPos(int X, int Y);
+
+            [DllImport("user32.dll")]
+            public static extern bool GetCursorPos(out Point pos);
+
+            [StructLayout(LayoutKind.Sequential)]
+            public struct Point
+            {
+                public int x;
+                public int y;
+
+                public static implicit operator Vector2(Point point) => new Vector2(point.x, point.y);
+            }
+        }
+    }
+}
---
+++ KK_Fix_MakerOptimization/MakerOptimization.cs
@@ -0,0 +1,118 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using Common;
+using Harmony;
+using System.ComponentModel;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Logger = BepInEx.Logger;
+
+namespace KK_Fix_MakerOptimization
+{
+    [BepInPlugin(GUID, PluginName, Metadata.PluginsVersion)]
+    public class MakerOptimization : BaseUnityPlugin
+    {
+        public const string GUID = "keelhauled.fixcompilation";
+        public const string PluginName = "Maker Optimization";
+
+        [DisplayName("Disable \"NEW\" indicator animation")]
+        [Description("Major performance improvement in maker if there are many new items. Almost no visual effect.")]
+        public static ConfigWrapper<bool> DisableNewAnimation { get; private set; }
+
+        [DisplayName("Disable \"NEW\" indicator for new items")]
+        [Description("Good performance improvement in maker if there are many new items.\n\nChanges take effect after maker restart.")]
+        public static ConfigWrapper<bool> DisableNewIndicator { get; private set; }
+
+        [DisplayName("Disable maker IK")]
+        [Description("Improves performance and reduces stuttering at the cost of not recalculating positions of some body parts.\n\n" +
+                     "Most noticeable on characters with wide hips where the hands are not moving with the hip line.\n\n" +
+                     "Warning: This setting will get reset to false if Stiletto is installed to avoid issues!\n\n" +
+                     "Changes take effect after game restart.")]
+        public static ConfigWrapper<bool> DisableIKCalc { get; private set; }
+
+        [DisplayName("Disable camera target (white focus ring)")]
+        [Description("Warning: This setting overrides any game setting that enables the ring.")]
+        public static ConfigWrapper<bool> DisableCameraTarget { get; private set; }
+
+        [DisplayName("Disable character name box in maker")]
+        [Description("Hides the name box in the bottom right part of the maker, giving you a clearer look at the character.")]
+        public static ConfigWrapper<bool> DisableCharaName { get; private set; }
+
+        [DisplayName("Deactivate hidden tabs in maker")]
+        [Description("Major performance improvement at the cost of slower switching between tabs in maker.\n\nChanges take effect after maker restart.")]
+        public static ConfigWrapper<bool> DisableHiddenTabs { get; private set; }
+
+        [DisplayName("Avoid yamadamod exceptions")]
+        [Description("Increases fps in certain situations where exceptions are being spammed. This fix may be unnecessary if you don't have sideloader.\n\n" +
+                     "Changes take effect after game restart.")]
+        public static ConfigWrapper<bool> EnableYamadamodFix { get; private set; }
+
+        [DisplayName("Manage cursor in maker")]
+        [Description("Lock and hide the cursor when moving the camera in maker.")]
+        public static ConfigWrapper<bool> ManageCursor { get; private set; }
+
+        public MakerOptimization()
+        {
+            DisableNewAnimation = new ConfigWrapper<bool>("DisableNewAnimation", this, true);
+            DisableNewIndicator = new ConfigWrapper<bool>("DisableNewIndicator", this, true);
+            DisableIKCalc = new ConfigWrapper<bool>("DisableIKCalc", this, true);
+            DisableCameraTarget = new ConfigWrapper<bool>("DisableCameraTarget", this, false);
+            DisableCharaName = new ConfigWrapper<bool>("DisableCharaName", this, true);
+            DisableHiddenTabs = new ConfigWrapper<bool>("DisableHiddenTabs", this, true);
+            EnableYamadamodFix = new ConfigWrapper<bool>("EnableYamadamodFix", this, true);
+            ManageCursor = new ConfigWrapper<bool>("ManageCursor", this, true);
+        }
+
+        protected void Awake()
+        {
+            SceneManager.sceneLoaded += SceneLoaded;
+            DisableCameraTarget.SettingChanged += (sender, args) => ApplyPatches();
+            DisableCharaName.SettingChanged += (sender, args) => ApplyPatches();
+
+            var harmony = HarmonyInstance.Create($"{GUID}.harmony");
+            Hooks.Patch(harmony);
+        }
+
+        private void Start()
+        {
+            if (DisableIKCalc.Value && BepInEx.Bootstrap.Chainloader.Plugins.Select(MetadataHelper.GetMetadata).Any(x => x.GUID == "com.essu.stiletto"))
+            {
+                DisableIKCalc.Value = false;
+                Logger.Log(LogLevel.Warning, "Stiletto detected, disabling the DisableIKCalc optimization");
+            }
+        }
+
+        private void SceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            ApplyPatches();
+
+            var cursorManager = gameObject.GetComponent<CursorManager>();
+            if (FindObjectOfType<CustomScene>())
+            {
+                if (!cursorManager) gameObject.AddComponent<CursorManager>();
+            }
+            else if (cursorManager)
+            {
+                Destroy(cursorManager);
+            }
+        }
+
+        private void ApplyPatches()
+        {
+            if (FindObjectOfType<StudioScene>())
+            {
+                GameObject.Find("StudioScene/Camera/Main Camera/CameraTarget")?.SetActive(!DisableCameraTarget.Value);
+            }
+            else if (FindObjectOfType<CustomScene>())
+            {
+                GameObject.Find("CustomScene/CamBase/Camera/CameraTarget")?.SetActive(!DisableCameraTarget.Value);
+                GameObject.Find("CustomScene/CustomRoot/FrontUIGroup/CustomUIGroup/CvsCharaName")?.SetActive(!DisableCharaName.Value);
+            }
+            else if (FindObjectOfType<HSceneProc>())
+            {
+                GameObject.Find("HScene/CameraBase/Camera/CameraTarget")?.SetActive(!DisableCameraTarget.Value);
+            }
+        }
+    }
+}
