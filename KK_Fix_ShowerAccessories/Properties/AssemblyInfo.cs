453f2f7775771c79e51aa35c3e0aa040206ed59a
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-06-18 11:22:45 -0500

    Added ShowerAccessoriesFix

---
+++ KK_Fix_ShowerAccessories/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KK_Fix_ShowerAccessories")]
+[assembly: AssemblyProduct("KK_Fix_ShowerAccessories")]
+[assembly: AssemblyDescription("Shower Accessory Fix for Koikatsu")]
+[assembly: Guid("abb91777-7231-47f7-bf4d-9144cc7b1764")]
---
+++ KK_Fix_ShowerAccessories/ShowerAccessories.cs
@@ -0,0 +1,34 @@
+﻿using ActionGame;
+using BepInEx;
+using Common;
+using Harmony;
+using System.Collections.Generic;
+
+namespace KK_Fix_ShowerAccessories
+{
+    /// <summary>
+    /// Prevents accessories from being disabled in the shower peeping mode
+    /// </summary>
+    [BepInPlugin(GUID, PluginName, Metadata.PluginsVersion)]
+    public class ShowerAccessories : BaseUnityPlugin
+    {
+        public const string GUID = "KK_Fix_ShowerAccessories";
+        public const string PluginName = "Shower Accessories Fix";
+
+        private void Main() => HarmonyInstance.Create(GUID).PatchAll(typeof(ShowerAccessories));
+
+        [HarmonyPrefix, HarmonyPatch(typeof(HSceneProc), "MapSameObjectDisable")]
+        public static void MapSameObjectDisable(HSceneProc __instance)
+        {
+            var map = (ActionMap)AccessTools.Field(typeof(HSceneProc), "map").GetValue(Singleton<HSceneProc>.Instance);
+            if (map.no == 52) //shower
+            {
+                var lstFemale = (List<ChaControl>)AccessTools.Field(typeof(HSceneProc), "lstFemale").GetValue(Singleton<HSceneProc>.Instance);
+
+                //Turn accessories back on and then turn off only Sub accessories
+                lstFemale[0].SetAccessoryStateAll(true);
+                lstFemale[0].SetAccessoryStateCategory(1, false);
+            }
+        }
+    }
+}
