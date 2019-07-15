982b993cdf9aa6bdd8ef6efb63a2acd4bd3a2172
ManlyMarco <dummyaccx@gmail.com>
2019-07-16 01:41:01 +0200

    Refactoring

--- KK_Fix_PersonalityCorrector/PersonalityCorrector.Hooks.cs
+++ KK_Fix_PersonalityCorrector/PersonalityCorrector.Hooks.cs
@@ -1,93 +1,26 @@
-﻿using System;
+﻿using ActionGame;
 using Harmony;
-using ActionGame;
-using BepInEx;
-using BepInEx.Logging;
 
 namespace KK_Fix_PersonalityCorrector
 {
-    internal class Hooks
+    public partial class PersonalityCorrector
     {
-        public static int DefaultPersonality = 8; //Pure
-
-        [HarmonyPostfix, HarmonyPatch(typeof(ChaControl), nameof(ChaControl.GetRandomFemaleCard))]
-        public static void GetRandomFemaleCard(ref ChaFileControl[] __result)
+        private class Hooks
         {
-            foreach (var chaFileControl in __result)
-                CheckPersonalityAndOverride(chaFileControl);
-        }
-
-        [HarmonyPrefix, HarmonyPatch(typeof(PreviewClassData), nameof(PreviewClassData.Set), new Type[] { typeof(SaveData.CharaData) })]
-        public static void SetClassChara(SaveData.CharaData charaData)
-        {
-            CheckPersonalityAndOverride(charaData.charFile);
-        }
-
-        private static void CheckPersonalityAndOverride(ChaFileControl chaFileControl)
-        {
-            switch (chaFileControl.parameter.personality)
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(ChaControl), nameof(ChaControl.GetRandomFemaleCard))]
+            public static void GetRandomFemaleCard(ref ChaFileControl[] __result)
             {
-                case 30: //0727 Free DLC
-                    if (!AssetBundleCheck.IsFile("etcetra/list/config/14.unity3d"))
-                    {
-                        ShowPersonalityMissingMessage(chaFileControl, "0727 Free DLC");
-                        chaFileControl.parameter.personality = DefaultPersonality;
-                    }
-                    break;
-                case 31: //0727 Paid DLC #1
-                    if (!AssetBundleCheck.IsFile("etcetra/list/config/15.unity3d"))
-                    {
-                        ShowPersonalityMissingMessage(chaFileControl, "0727 Summer Expansion");
-                        chaFileControl.parameter.personality = DefaultPersonality;
-                    }
-                    break;
-                case 32: //0727 Paid DLC #1
-                    if (!AssetBundleCheck.IsFile("etcetra/list/config/16.unity3d"))
-                    {
-                        ShowPersonalityMissingMessage(chaFileControl, "0727 Summer Expansion");
-                        chaFileControl.parameter.personality = DefaultPersonality;
-                    }
-                    break;
-                case 33: //0727 Paid DLC #1
-                    if (!AssetBundleCheck.IsFile("etcetra/list/config/17.unity3d"))
-                    {
-                        ShowPersonalityMissingMessage(chaFileControl, "0727 Summer Expansion");
-                        chaFileControl.parameter.personality = DefaultPersonality;
-                    }
-                    break;
-                case 34:
-                case 35:
-                case 36:
-                case 37: //1221 Paid DLC #2
-                    if (!AssetBundleCheck.IsFile("etcetra/list/config/20.unity3d"))
-                    {
-                        ShowPersonalityMissingMessage(chaFileControl, "1221 AfterSchool Expansion");
-                        chaFileControl.parameter.personality = DefaultPersonality;
-                    }
-                    break;
-                case 38: //EmotionCreators preorder bonus personality
-                    if (!AssetBundleCheck.IsFile("etcetra/list/config/50.unity3d"))
-                    {
-                        ShowPersonalityMissingMessage(chaFileControl, "EmotionCreators preorder bonus");
-                        chaFileControl.parameter.personality = DefaultPersonality;
-                    }
-                    break;
-                case 80:
-                case 81:
-                case 82:
-                case 83:
-                case 84:
-                case 85:
-                case 86: //Story character personalities added by a mod
-                    chaFileControl.parameter.personality = DefaultPersonality;
-                    Logger.Log(LogLevel.Message, chaFileControl.parameter.fullname + " - Modded personality " + chaFileControl.parameter.personality + " is not compatible with story mode, resetting to default");
-                    break;
+                foreach (var chaFileControl in __result)
+                    CheckPersonalityAndOverride(chaFileControl);
             }
-        }
 
-        private static void ShowPersonalityMissingMessage(ChaFileControl chaFile, string dlcName)
-        {
-            Logger.Log(LogLevel.Message, chaFile.parameter.fullname + " - Personality " + chaFile.parameter.personality + " from " + dlcName + " is missing, resetting to default");
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(PreviewClassData), nameof(PreviewClassData.Set), new[] { typeof(SaveData.CharaData) })]
+            public static void SetClassChara(SaveData.CharaData charaData)
+            {
+                CheckPersonalityAndOverride(charaData.charFile);
+            }
         }
     }
 }
--- KK_Fix_PersonalityCorrector/PersonalityCorrector.cs
+++ KK_Fix_PersonalityCorrector/PersonalityCorrector.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
 using Common;
 using Harmony;
 
@@ -8,14 +9,90 @@ namespace KK_Fix_PersonalityCorrector
     /// Changes any invalid personalities to the "Pure" personality to prevent the game from breaking when adding them to the class
     /// </summary>
     [BepInPlugin(GUID, PluginName, Metadata.PluginsVersion)]
-    public class PersonalityCorrector : BaseUnityPlugin
+    public partial class PersonalityCorrector : BaseUnityPlugin
     {
         public const string GUID = "com.deathweasel.bepinex.personalitycorrector";
         public const string PluginName = "Personality Corrector";
 
+        public static int DefaultPersonality = 8; // 8 - Pure
+
         private void Awake()
         {
             HarmonyInstance.Create(GUID).PatchAll(typeof(Hooks));
+        }
+
+        public static void CheckPersonalityAndOverride(ChaFileControl chaFileControl)
+        {
+            switch (chaFileControl.parameter.personality)
+            {
+                case 30: //0727 Free DLC
+                    if (!AssetBundleCheck.IsFile("etcetra/list/config/14.unity3d"))
+                    {
+                        ShowPersonalityMissingMessage(chaFileControl, "0727 Free DLC");
+                        chaFileControl.parameter.personality = DefaultPersonality;
+                    }
+                    break;
+                case 31: //0727 Paid DLC #1
+                    if (!AssetBundleCheck.IsFile("etcetra/list/config/15.unity3d"))
+                    {
+                        ShowPersonalityMissingMessage(chaFileControl, "0727 Summer Expansion");
+                        chaFileControl.parameter.personality = DefaultPersonality;
+                    }
+                    break;
+                case 32: //0727 Paid DLC #1
+                    if (!AssetBundleCheck.IsFile("etcetra/list/config/16.unity3d"))
+                    {
+                        ShowPersonalityMissingMessage(chaFileControl, "0727 Summer Expansion");
+                        chaFileControl.parameter.personality = DefaultPersonality;
+                    }
+                    break;
+                case 33: //0727 Paid DLC #1
+                    if (!AssetBundleCheck.IsFile("etcetra/list/config/17.unity3d"))
+                    {
+                        ShowPersonalityMissingMessage(chaFileControl, "0727 Summer Expansion");
+                        chaFileControl.parameter.personality = DefaultPersonality;
+                    }
+                    break;
+                case 34:
+                case 35:
+                case 36:
+                case 37: //1221 Paid DLC #2
+                    if (!AssetBundleCheck.IsFile("etcetra/list/config/20.unity3d"))
+                    {
+                        ShowPersonalityMissingMessage(chaFileControl, "1221 AfterSchool Expansion");
+                        chaFileControl.parameter.personality = DefaultPersonality;
+                    }
+                    break;
+                case 38: //EmotionCreators preorder bonus personality
+                    if (!AssetBundleCheck.IsFile("etcetra/list/config/50.unity3d"))
+                    {
+                        ShowPersonalityMissingMessage(chaFileControl, "EmotionCreators preorder bonus");
+                        chaFileControl.parameter.personality = DefaultPersonality;
+                    }
+                    break;
+                case 80:
+                case 81:
+                case 82:
+                case 83:
+                case 84:
+                case 85:
+                case 86: //Story character personalities added by a mod
+                    ShowInvalidModdedPersonality(chaFileControl);
+                    chaFileControl.parameter.personality = DefaultPersonality;
+                    break;
+            }
+        }
+
+        private static void ShowInvalidModdedPersonality(ChaFileControl chaFileControl)
+        {
+            Logger.Log(LogLevel.Message, chaFileControl.parameter.fullname + " - Modded personality " +
+                chaFileControl.parameter.personality + " is not compatible with story mode, resetting to default");
+        }
+
+        private static void ShowPersonalityMissingMessage(ChaFileControl chaFile, string dlcName)
+        {
+            Logger.Log(LogLevel.Message, chaFile.parameter.fullname + " - Personality " +
+                chaFile.parameter.personality + " from " + dlcName + " is missing, resetting to default");
         }
     }
 }
