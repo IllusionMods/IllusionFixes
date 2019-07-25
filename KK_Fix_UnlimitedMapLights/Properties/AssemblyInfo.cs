a51820c25ac3768fac56816affd17291527839e0
Keelhauled <keelhauledhs@gmail.com>
2019-07-26 01:02:39 +0300

    UnlimitedMapLights

---
+++ KK_Fix_UnlimitedMapLights/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("KK_Fix_UnlimitedMapLights")]
+[assembly: AssemblyProduct("KK_Fix_UnlimitedMapLights")]
+[assembly: AssemblyDescription("Allows adding an unlimited amount of map lights to studio")]
