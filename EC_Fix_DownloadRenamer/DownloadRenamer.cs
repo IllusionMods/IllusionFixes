516ac92ac4f8f2193b51f3e979b695739e0d730b
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-08-11 18:15:42 -0500

    Add EC_Fix_DownloadRenamer

---
+++ EC_Fix_DownloadRenamer/DownloadRenamer.cs
@@ -0,0 +1,71 @@
+﻿using System.IO;
+using BepInEx;
+using BepInEx.Configuration;
+using Common;
+using HarmonyLib;
+using UploaderSystem;
+
+namespace IllusionFixes
+{
+    [BepInPlugin(GUID, PluginName, Metadata.PluginsVersion)]
+    public class DownloadRenamer : BaseUnityPlugin
+    {
+        public const string GUID = "EC_Fix_DownloadRenamer";
+        public const string PluginName = "Download Renamer";
+        public static ConfigWrapper<bool> EnambleRenaming { get; private set; }
+
+        private void Start()
+        {
+            BepInEx.Harmony.HarmonyWrapper.PatchAll(typeof(DownloadRenamer));
+            EnambleRenaming = Utilities.FixesConfig.Wrap(Utilities.ConfigSectionTweaks, "Rename downloads",
+                "When enabled, maps, scenes, poses, and characters downloaded in game will have their file names changed to match the ones on the Illusion website.", true);
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(NetUIControl), "SaveDownloadFile")]
+        public static bool SaveDownloadFilePrefix(NetUIControl __instance, byte[] bytes, NetworkInfo.BaseIndex info)
+        {
+            if (!EnambleRenaming.Value) return true;
+
+            var fileName = GetFilename(__instance.dataType, info);
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                // Unknown type, fallback to the original function
+                return true;
+            }
+
+            File.WriteAllBytes(fileName, bytes);
+
+            return false;
+        }
+
+        /// <summary>
+        /// Generate names to match the web downloader, which are more sensible.
+        /// The original names reflect the time of download, which is not only redundant
+        /// information, but also allows redundant copies to be downloaded.
+        /// </summary>
+        private static string GetFilename(int dataType, NetworkInfo.BaseIndex info)
+        {
+            switch (dataType)
+            {
+                case 0:
+                    var charaInfo = info as NetworkInfo.CharaInfo;
+                    return GetFileName(charaInfo?.sex == 1 ? "chara/female" : "chara/male", "emocre_chara_", info.idx);
+                case 1:
+                    return GetFileName("map/data", "emocre_map_", info.idx);
+                case 2:
+                    return GetFileName("pose/data", "emocre_pose_", info.idx);
+                case 3:
+                    return GetFileName("edit/scene", "emocre_scene_", info.idx);
+                default:
+                    Utilities.Logger.LogWarning($"Unknown download file type {dataType}, can't rename");
+                    return null;
+            }
+        }
+
+        private static string GetFileName(string dir, string prefix, int index)
+        {
+            return UserData.Create(dir) + prefix + index.ToString("D7") + ".png";
+        }
+    }
+}
---
+++ EC_Fix_DownloadRenamer/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("EC_Fix_DownloadRenamer")]
+[assembly: AssemblyProduct("EC_Fix_DownloadRenamer")]
+[assembly: AssemblyDescription("Rename downloaded files")]
