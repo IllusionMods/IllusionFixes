2dd9a9ac21fefb0814642523986edc9ae91a71af
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-10-28 13:39:52 -0500

    Add AI Studio name

--- src/Common/CommonCode.cs
+++ src/Common/CommonCode.cs
@@ -4,7 +4,7 @@ namespace Common
 {
     internal static class CommonCode
     {
-        internal static bool InsideStudio => Application.productName == "CharaStudio";
+        internal static bool InsideStudio => Application.productName == "CharaStudio" || Application.productName == "StudioNEOV2";
         internal static bool InsideKoikatsuParty => Application.productName == "Koikatsu Party";
     }
 }
