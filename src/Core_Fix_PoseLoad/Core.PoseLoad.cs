3074218c0a7fd43fbe30b49a59965b3ffccf0248
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-10-29 10:44:49 -0500

    Add holding ctrl to correct hand pose for KK poses in AI

--- src/Core_Fix_PoseLoad/Core.PoseLoad.cs
+++ src/Core_Fix_PoseLoad/Core.PoseLoad.cs
@@ -18,9 +18,16 @@ namespace IllusionFixes
             [HarmonyPrefix, HarmonyPatch(typeof(PauseCtrl.FileInfo), nameof(PauseCtrl.FileInfo.Apply))]
             internal static bool Apply(PauseCtrl.FileInfo __instance, OCIChar _char)
             {
+                //AI and KK pose files are apparently indistinguishable from each other
+                //If the user is holding ctrl while loading the pose treat it as a KK pose for the purposes of correcting the right hand
+                bool KKPose = false;
+                if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+                    KKPose = true;
+
                 //326 is a bone that exists in HS but not KK, check that to see if this is a loaded HS pose
-                bool HS = __instance.dicFK.Keys.Any(x => x == 326);
-                if (!HS) return true;
+                bool HSPose = __instance.dicFK.Keys.Any(x => x == 326);
+
+                if (!HSPose && !KKPose) return true;
 
                 #region Vanilla Code
                 _char.LoadAnime(__instance.group, __instance.category, __instance.no, __instance.normalizedTime);
@@ -40,18 +47,21 @@ namespace IllusionFixes
                 {
                     int key = item2.Key;
 
-                    //Breasts translated from HS to KK
-                    if (key == 326) key = 53;
-                    if (key == 327) key = 54;
-                    if (key == 328) key = 55;
-                    if (key == 329) key = 56;
-                    if (key == 330) key = 57;
+                    if (HSPose)
+                    {
+                        //Breasts translated from HS to KK
+                        if (key == 326) key = 53;
+                        if (key == 327) key = 54;
+                        if (key == 328) key = 55;
+                        if (key == 329) key = 56;
+                        if (key == 330) key = 57;
 
-                    if (key == 332) key = 59;
-                    if (key == 333) key = 60;
-                    if (key == 334) key = 61;
-                    if (key == 335) key = 62;
-                    if (key == 336) key = 63;
+                        if (key == 332) key = 59;
+                        if (key == 333) key = 60;
+                        if (key == 334) key = 61;
+                        if (key == 335) key = 62;
+                        if (key == 336) key = 63;
+                    }
 
                     if (_char.oiCharInfo.bones.TryGetValue(key, out var oIBoneInfo))
                     {
@@ -65,16 +75,29 @@ namespace IllusionFixes
 
                         if (key == 24 || key == 27 || key == 30 || key == 33 || key == 36)
                             item2.Value.rot = new Vector3(item2.Value.rot.x, -item2.Value.rot.y, -item2.Value.rot.z);
+#elif AI
+                        if (KKPose && !HSPose)
+                        {
+                            //Correct the right hand
+                            if (key == 22 || key == 25 || key == 28 || key == 31 || key == 34)
+                                item2.Value.rot = new Vector3(-item2.Value.rot.x, 180 + item2.Value.rot.y, 180 - item2.Value.rot.z);
+
+                            if (key == 23 || key == 26 || key == 29 || key == 32 || key == 35)
+                                item2.Value.rot = new Vector3(item2.Value.rot.x, -item2.Value.rot.y, -item2.Value.rot.z);
+
+                            if (key == 24 || key == 27 || key == 30 || key == 33 || key == 36)
+                                item2.Value.rot = new Vector3(item2.Value.rot.x, -item2.Value.rot.y, -item2.Value.rot.z);
+                        }
 #endif
 
                         oIBoneInfo.changeAmount.Copy(item2.Value);
                     }
                 }
 
-#region Vanilla Code
+                #region Vanilla Code
                 for (int k = 0; k < __instance.expression.Length; k++)
                     _char.EnableExpressionCategory(k, __instance.expression[k]);
-#endregion
+                #endregion
 
                 return false;
             }
