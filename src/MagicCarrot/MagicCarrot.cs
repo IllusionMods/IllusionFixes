b15bfd2eee0f8e536025c410d54c0ea4b8f626a9
DeathWeasel1337 <deathweasel1337@gmail.com>
2019-10-29 14:24:19 -0500

    Add MagicCarrot

---
+++ src/MagicCarrot/MagicCarrot.cs
@@ -0,0 +1,23 @@
+﻿using Mono.Cecil;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MagicCarrot
+{
+    // Fixes random hangups in Illusion's new game
+    // Why? I dunno lol
+    public static class MagicCarrotPatch
+    {
+        public static IEnumerable<string> TargetDLLs { get; } = new[] { "Sirenix.Utilities.dll", "Sirenix.Serialization.dll" };
+
+        public static void Patch(AssemblyDefinition ad)
+        {
+            var assemblyUtilities = ad.MainModule.Types.FirstOrDefault(t => t.Name == "AssemblyUtilities");
+            var cctor = assemblyUtilities?.Methods.FirstOrDefault(m => m.Name == ".cctor");
+
+            if (cctor == null)
+                return;
+            assemblyUtilities.Methods.Remove(cctor);
+        }
+    }
+}
