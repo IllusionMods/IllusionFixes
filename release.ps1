80050092c50ee821271141dd0e002f997edb90d8
ManlyMarco <dummyaccx@gmail.com>
2019-11-08 21:56:38 +0100

    Added a release script

---
+++ release.ps1
@@ -0,0 +1,44 @@
+if ($PSScriptRoot -match '.+?\\bin\\?') {
+    $dir = $PSScriptRoot + "\"
+}
+else {
+    $dir = $PSScriptRoot + "\bin\"
+}
+
+$copy = $dir + "\copy\BepInEx" 
+
+$ver = [System.Diagnostics.FileVersionInfo]::GetVersionInfo((Get-ChildItem -Path ($dir + "\BepInEx\patchers\*") -Force)[0]).FileVersion.ToString()
+
+New-Item -ItemType Directory -Force -Path ($dir + "\out")  
+
+Remove-Item -Force -Path ($dir + "\copy") -Recurse -ErrorAction SilentlyContinue
+New-Item -ItemType Directory -Force -Path ($copy + "\plugins\IllusionFixes")
+New-Item -ItemType Directory -Force -Path ($copy + "\patchers")
+
+Copy-Item -Path ($dir + "\BepInEx\plugins\IllusionFixes\AI_*.*") -Destination ($copy + "\plugins\IllusionFixes") -Force 
+Copy-Item -Path ($dir + "\BepInEx\patchers\*") -Destination ($copy + "\patchers") -Force 
+
+Compress-Archive -Path $copy -Force -CompressionLevel "Optimal" -DestinationPath ($dir + "out\" + "IllusionFixes_AIGirl_" + $ver + ".zip")
+
+
+Remove-Item -Force -Path ($dir + "\copy") -Recurse
+New-Item -ItemType Directory -Force -Path ($copy + "\plugins\IllusionFixes")
+New-Item -ItemType Directory -Force -Path ($copy + "\patchers") 
+
+Copy-Item -Path ($dir + "\BepInEx\plugins\IllusionFixes\EC_*.*") -Destination ($copy + "\plugins\IllusionFixes") -Force 
+Copy-Item -Path ($dir + "\BepInEx\patchers\*") -Destination ($copy + "\patchers") -Force 
+
+Compress-Archive -Path $copy -Force -CompressionLevel "Optimal" -DestinationPath ($dir + "out\" + "IllusionFixes_EmotionCreators_" + $ver + ".zip")
+
+
+Remove-Item -Force -Path ($dir + "\copy") -Recurse
+New-Item -ItemType Directory -Force -Path ($copy + "\plugins\IllusionFixes")  
+New-Item -ItemType Directory -Force -Path ($copy + "\patchers") 
+
+Copy-Item -Path ($dir + "\BepInEx\plugins\IllusionFixes\KK_*.*") -Destination ($copy + "\plugins\IllusionFixes") -Force 
+#Copy-Item -Path ($dir + "\BepInEx\patchers\*") -Destination ($copy + "\patchers") -Force 
+
+Compress-Archive -Path $copy -Force -CompressionLevel "Optimal" -DestinationPath ($dir + "out\" + "IllusionFixes_Koikatsu_" + $ver + ".zip")
+
+
+Remove-Item -Force -Path ($dir + "\copy") -Recurse
