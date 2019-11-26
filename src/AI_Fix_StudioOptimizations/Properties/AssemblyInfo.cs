ffd5a2ceefa11e40367571933bf428c6dab05669
ManlyMarco <dummyaccx@gmail.com>
2019-11-26 19:49:14 +0100

    Added stubs for studio optimizations

---
+++ src/AI_Fix_StudioOptimizations/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using IllusionFixes;
+
+[assembly: AssemblyTitle(StudioOptimizations.GUID)]
+[assembly: AssemblyProduct(StudioOptimizations.GUID)]
+[assembly: AssemblyDescription(StudioOptimizations.PluginName)]
---
+++ src/KK_Fix_StudioOptimizations/StudioOptimizations.cs
@@ -0,0 +1,8 @@
+﻿namespace IllusionFixes
+{
+    public class StudioOptimizations 
+    {
+        public const string GUID = "KK_Fix_StudioOptimizations";
+        public const string PluginName = "Studio Optimizations";
+    }
+}
