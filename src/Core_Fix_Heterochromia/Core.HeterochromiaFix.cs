3e0a09b0dd0dafad7328d229411045a176471441
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-01-03 18:13:41 -0600

    Add HeterochromiaFix

---
+++ src/Core_Fix_Heterochromia/Core.HeterochromiaFix.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx.Harmony;
+using BepInEx.Logging;
+
+namespace IllusionFixes
+{
+    /// <summary>
+    /// Sets the "Edit to Eye" radio buttons to "left" for characters with different pupils or gradients so that these things load properly in the character maker.
+    /// </summary>
+    public partial class HeterochromiaFix
+    {
+        public const string PluginName = "Character Maker Heterochromia Fix";
+        internal static new ManualLogSource Logger;
+
+        internal void Main()
+        {
+            Logger = base.Logger;
+            HarmonyWrapper.PatchAll(typeof(Hooks));
+        }
+    }
+}
---
+++ src/KK_Fix_Heterochromia/KK.HeterochromiaFix.cs
@@ -0,0 +1,11 @@
+﻿using BepInEx;
+using Common;
+
+namespace IllusionFixes
+{
+    [BepInPlugin(GUID, PluginName, Metadata.PluginsVersion)]
+    public partial class HeterochromiaFix : BaseUnityPlugin
+    {
+        public const string GUID = "KK_Fix_Heterochromia";
+    }
+}
