8ec8e86618335af5bbdca31cfd1333ebcf5cf9b7
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-01-04 18:57:24 -0600

    Add ResourceUnloadOptimizations for PlayHome

---
+++ src/PH_Fix_ResourceUnloadOptimizations/PH.ResourceUnloadOptimizations.cs
@@ -0,0 +1,11 @@
+﻿using BepInEx;
+using Common;
+
+namespace IllusionFixes
+{
+    [BepInPlugin(GUID, PluginName, Metadata.PluginsVersion)]
+    public partial class ResourceUnloadOptimizations : BaseUnityPlugin
+    {
+        public const string GUID = "PH_Fix_ResourceUnloadOptimizations";
+    }
+}
