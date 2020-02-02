1b2d2f104575fb8673316e829c7ef0333b0d3077
ManlyMarco <dummyaccx@gmail.com>
2020-02-02 22:04:44 +0100

    Version up to 1.9

--- src/Common/Metadata.cs
+++ src/Common/Metadata.cs
@@ -6,6 +6,6 @@
         /// Version number used by all plugins. Must be convertible to <see cref="System.Version"/>
         /// Uses ReleaseVersion.HotfixVersion format.
         /// </summary>
-        public const string PluginsVersion = "1.8";
+        public const string PluginsVersion = "1.9";
     }
 }
