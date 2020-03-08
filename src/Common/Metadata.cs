a8993c2184ba688d308426b465819b2c86639995
ManlyMarco <dummyaccx@gmail.com>
2020-03-08 12:59:24 +0100

    Version up to 1.10.1

--- src/Common/Metadata.cs
+++ src/Common/Metadata.cs
@@ -6,6 +6,6 @@
         /// Version number used by all plugins. Must be convertible to <see cref="System.Version"/>
         /// Uses ReleaseVersion.HotfixVersion format.
         /// </summary>
-        public const string PluginsVersion = "1.10";
+        public const string PluginsVersion = "1.10.1";
     }
 }
