ce40abb78a818fe823a4644d00e339b9ea321216
ManlyMarco <dummyaccx@gmail.com>
2020-05-10 22:19:04 +0200

    Prevent crashes in japanese AIS if language is set to other than 0 in setup.xml

--- src/AI_Fix_SettingsVerifier/AI.SettingsVerifier.cs
+++ src/AI_Fix_SettingsVerifier/AI.SettingsVerifier.cs
@@ -1,5 +1,9 @@
-﻿using BepInEx;
+﻿using System;
+using System.Linq;
+using BepInEx;
 using Common;
+using HarmonyLib;
+using Manager;
 
 namespace IllusionFixes
 {
@@ -9,5 +13,21 @@ namespace IllusionFixes
     public partial class SettingsVerifier : BaseUnityPlugin
     {
         public const string GUID = "AI_Fix_SettingsVerifier";
+
+        internal static partial class Hooks
+        {
+            /// <summary>
+            /// Check if the loaded language is supported, if not reset to a sane value
+            /// </summary>
+            [HarmonyPostfix, HarmonyPatch(typeof(GameSystem), "LoadLanguage")]
+            internal static void CheckLanguage(GameSystem __instance)
+            {
+                if (!Enum.GetValues(typeof(GameSystem.Language)).Cast<GameSystem.Language>().Contains(__instance.language))
+                {
+                    UnityEngine.Debug.LogWarning("Unsupported language was set, resetting to Japanese");
+                    Traverse.Create(__instance).Property(nameof(__instance.language)).SetValue(GameSystem.Language.Japanese);
+                }
+            }
+        }
     }
 }
