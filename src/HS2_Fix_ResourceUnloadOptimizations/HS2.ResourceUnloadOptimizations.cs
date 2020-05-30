4a8b909c2479ecf59a2a81e33b053164f3ff8d3c
ManlyMarco <dummyaccx@gmail.com>
2020-05-30 23:08:19 +0200

    Added HS2_Fix_ResourceUnloadOptimizations

---
+++ src/HS2_Fix_ResourceUnloadOptimizations/HS2.ResourceUnloadOptimizations.cs
@@ -0,0 +1,11 @@
+﻿using BepInEx;
+using Common;
+
+namespace IllusionFixes
+{
+    [BepInPlugin(GUID, PluginName, Metadata.PluginsVersion)]
+    public partial class ResourceUnloadOptimizations : BaseUnityPlugin
+    {
+        public const string GUID = "HS2_Fix_ResourceUnloadOptimizations";
+    }
+}
