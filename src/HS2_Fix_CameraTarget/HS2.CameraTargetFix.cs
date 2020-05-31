64510f61746daf896b47572d0324102b17063ad9
ManlyMarco <dummyaccx@gmail.com>
2020-05-31 18:29:00 +0200

    Added HS2_Fix_CameraTarget

---
+++ src/HS2_Fix_CameraTarget/HS2.CameraTargetFix.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using CharaCustom;
+using Common;
+using HarmonyLib;
+
+namespace IllusionFixes
+{
+    [BepInIncompatibility("keelhauled.cameratargetfix")]
+    [BepInProcess(Constants.GameProcessName)]
+    [BepInProcess(Constants.StudioProcessName)]
+    [BepInPlugin(GUID, PluginName, Metadata.PluginsVersion)]
+    public class CameraTargetFix : CameraTargetFixCore
+    {
+        public const string GUID = "AI_Fix_CameraTarget";
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            if (Paths.ProcessName == Constants.StudioProcessName)
+            {
+                Harmony.Patch(typeof(Studio.CameraControl).GetMethod("InternalUpdateCameraState", AccessTools.all),
+                              transpiler: new HarmonyMethod(typeof(CameraTargetFixCore).GetMethod(nameof(StudioPatch), AccessTools.all)));
+            }
+            else
+            {
+                Harmony.Patch(typeof(CustomControl).GetMethod("Start", AccessTools.all),
+                              postfix: new HarmonyMethod(GetType().GetMethod(nameof(MakerPatch), AccessTools.all)));
+            }
+        }
+
+        private static void MakerPatch() => CustomBase.Instance.centerDraw = Manager.Config.CameraData.Look;
+    }
+}
