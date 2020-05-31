1e5dc58ad9428cdf61084000bc5a37c30c76f334
ManlyMarco <dummyaccx@gmail.com>
2020-05-31 17:46:48 +0200

    Added HS2_Fix_InvalidSceneFileProtection

---
+++ src/HS2_Fix_InvalidSceneFileProtection/InvalidSceneFileProtection.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using Common;
+using System.Text;
+
+namespace IllusionFixes
+{
+    [BepInProcess(Constants.StudioProcessName)]
+    [BepInPlugin(GUID, PluginName, Metadata.PluginsVersion)]
+    public partial class InvalidSceneFileProtection : BaseUnityPlugin
+    {
+        public const string GUID = "AI_Fix_InvalidSceneFileProtection";
+
+        private static readonly byte[][] ValidStudioTokens = { Encoding.UTF8.GetBytes("【StudioNEOV2】"), Encoding.UTF8.GetBytes("【KStudio】") };
+    }
+}
