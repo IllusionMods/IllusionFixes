5538fb002c9db6df881c026dec04fd0f3390c89e
ManlyMarco <dummyaccx@gmail.com>
2020-05-31 18:23:56 +0200

    Added HS2_Fix_ExpandShaderDropdown

---
+++ src/HS2_Fix_ExpandShaderDropdown/HS2.ExpandShaderDropdown.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using BepInEx;
+using Common;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace IllusionFixes
+{
+    [BepInProcess(Constants.StudioProcessName)]
+    [BepInPlugin(GUID, PluginName, Metadata.PluginsVersion)]
+    public class ExpandShaderDropdown : BaseUnityPlugin
+    {
+        public const string GUID = "HS2_Fix_ExpandShaderDropdown";
+        public const string PluginName = "Fix Shader Dropdown Menu";
+
+        private void Awake()
+        {
+            if (IncompatiblePluginDetector.AnyIncompatiblePlugins()) return;
+
+            SceneManager.sceneLoaded += (s, m) => StartCoroutine(DelayedStart());
+        }
+
+        private static IEnumerator DelayedStart()
+        {
+            // Needed to let UI load
+            yield return null;
+
+            if (Studio.Studio.IsInstance() && Manager.Scene.LoadSceneName == "Studio")
+            {
+                var traverse = Traverse.Create(Studio.Studio.Instance.systemButtonCtrl);
+                var tmpDropdownLut = traverse.Field("colorGradingInfo").Field("dropdownLookupTexture").GetValue<Dropdown>();
+                tmpDropdownLut.template.sizeDelta = new Vector2(0, 950);
+                var tmpDropdownRamp = traverse.Field("reflectionProbeInfo").Field("dropdownCubemap").GetValue<Dropdown>();
+                tmpDropdownRamp.template.sizeDelta = new Vector2(0, 800);
+            }
+        }
+    }
+}
--- src/HS2_Fix_InvalidSceneFileProtection/InvalidSceneFileProtection.cs
+++ src/HS2_Fix_InvalidSceneFileProtection/InvalidSceneFileProtection.cs
@@ -8,7 +8,7 @@ namespace IllusionFixes
     [BepInPlugin(GUID, PluginName, Metadata.PluginsVersion)]
     public partial class InvalidSceneFileProtection : BaseUnityPlugin
     {
-        public const string GUID = "AI_Fix_InvalidSceneFileProtection";
+        public const string GUID = "HS2_Fix_InvalidSceneFileProtection";
 
         private static readonly byte[][] ValidStudioTokens = { Encoding.UTF8.GetBytes("【StudioNEOV2】"), Encoding.UTF8.GetBytes("【KStudio】") };
     }
