161dcf3e67bcba3eefe74a28f5e81c1dd9bb16bb
ManlyMarco <dummyaccx@gmail.com>
2020-05-31 18:36:21 +0200

    Added HS2_Fix_MakerOptimizations

---
+++ src/HS2_Fix_MakerOptimizations/HS2.MakerOptimizations.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using CharaCustom;
+using Common;
+using UnityEngine.SceneManagement;
+
+namespace IllusionFixes
+{
+    [BepInProcess(Constants.GameProcessName)]
+    [BepInPlugin(GUID, PluginName, Metadata.PluginsVersion)]
+    public partial class MakerOptimizations : BaseUnityPlugin
+    {
+        public const string GUID = "HS2_Fix_MakerOptimizations";
+        public const string PluginName = "Maker Optimizations";
+
+        public static ConfigEntry<bool> ManageCursor { get; private set; }
+
+        internal void Awake()
+        {
+            if (IncompatiblePluginDetector.AnyIncompatiblePlugins())
+                return;
+
+            ManageCursor = Config.Bind(Utilities.ConfigSectionTweaks, "Manage cursor in maker", true, new ConfigDescription("Lock and hide the cursor when moving the camera in maker."));
+
+            SceneManager.sceneLoaded += SceneLoaded;
+        }
+
+        private void SceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            var cursorManager = gameObject.GetComponent<CursorManager>();
+
+            if (FindObjectOfType<CustomBase>())
+            {
+                if (!cursorManager)
+                    gameObject.AddComponent<CursorManager>();
+            }
+            else if (cursorManager)
+            {
+                Destroy(cursorManager);
+            }
+        }
+    }
+}
---
+++ src/HS2_Fix_MakerOptimizations/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle(IllusionFixes.MakerOptimizations.GUID)]
+[assembly: AssemblyProduct(IllusionFixes.MakerOptimizations.GUID)]
+[assembly: AssemblyDescription(IllusionFixes.MakerOptimizations.PluginName)]
