ec888d2bb3875bca1e6810ed9cd63c14f13ef7cf
ManlyMarco <dummyaccx@gmail.com>
2020-05-31 23:07:48 +0200

    Fixed patch not working in hs2

--- src/Core_Fix_SettingsVerifier/Core.SettingsVerifier.Hooks.cs
+++ src/Core_Fix_SettingsVerifier/Core.SettingsVerifier.Hooks.cs
@@ -36,7 +36,11 @@ namespace IllusionFixes
             /// <summary>
             /// Run the code for reading setup.xml when inside studio. Done in a Manager.Config.Start hook because the xmlRead method needs stuff to be initialized first.
             /// </summary>
+#if HS2
+            [HarmonyPostfix, HarmonyPatch(typeof(Manager.Config), MethodType.StaticConstructor)]
+#else
             [HarmonyPostfix, HarmonyPatch(typeof(Manager.Config), "Start")]
+#endif
             internal static void ManagerConfigStart()
             {
                 if (CommonCode.InsideStudio)
