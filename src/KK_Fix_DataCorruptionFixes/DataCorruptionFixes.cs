1bcd26a1e2232bf52ba3875a3309ddaac0d83f61
ManlyMarco <dummyaccx@gmail.com>
2020-06-20 01:20:05 +0200

    Added additional hooks to KK_Fix_DataCorruptionFixes

--- src/KK_Fix_DataCorruptionFixes/DataCorruptionFixes.cs
+++ src/KK_Fix_DataCorruptionFixes/DataCorruptionFixes.cs
@@ -8,6 +8,8 @@ using BepInEx.Logging;
 using ChaCustom;
 using Common;
 using HarmonyLib;
+using Studio;
+using UnityEngine;
 
 namespace IllusionFixes
 {
@@ -16,8 +18,8 @@ namespace IllusionFixes
     [BepInPlugin(GUID, PluginName, Metadata.PluginsVersion)]
     public class DataCorruptionFixes : BaseUnityPlugin
     {
-        public const string GUID = "KK_Fix_LoadingFixes";
-        public const string PluginName = "Game Data Corruption Fixes";
+        public const string GUID = "KK_Fix_DataCorruptionFixes";
+        public const string PluginName = "Game and Studio Data Corruption Fixes";
 
         private static new ManualLogSource Logger;
 
@@ -34,18 +36,19 @@ namespace IllusionFixes
 
         [HarmonyFinalizer]
         [HarmonyPatch(typeof(GlobalSaveData), nameof(GlobalSaveData.Load))]
+        [HarmonyPatch(typeof(SaveData), nameof(SaveData.Load), typeof(string), typeof(string))]
         [HarmonyPatch(typeof(RankingData), nameof(RankingData.Load))]
         [HarmonyPatch(typeof(WeddingData), nameof(WeddingData.Load))]
         [HarmonyPatch(typeof(MemoriesData), nameof(MemoriesData.Load))]
         [HarmonyPatch(typeof(DownloadScene), nameof(DownloadScene.LoadCacheSetting))] // uploader card caching setting
-        [HarmonyPatch(typeof(ChaListControl), nameof(ChaListControl.LoadItemID))] // checked items
+        [HarmonyPatch(typeof(ChaListControl), nameof(ChaListControl.LoadItemID))] // seen items
         [HarmonyPatch(typeof(CustomBase.CustomSettingSave), nameof(CustomBase.CustomSettingSave.Load))] // maker config
         private static Exception CatchCrash(MethodBase __originalMethod, Exception __exception)
         {
             if (__exception != null)
             {
-                Logger.Log(LogLevel.Error | LogLevel.Message, $"Corrupted save file detected in {__originalMethod.DeclaringType?.Name}, some progress might be lost.");
-                Logger.LogError(__exception);
+                Logger.Log(LogLevel.Warning | LogLevel.Message, $"Corrupted save file detected in {__originalMethod.DeclaringType?.Name}, some progress might be lost.");
+                Logger.LogWarning(__exception);
             }
 
             return null;
@@ -57,9 +60,9 @@ namespace IllusionFixes
         {
             if (__exception != null)
             {
-                Logger.Log(LogLevel.Error | LogLevel.Message, "ADV memory file was corrupted, you might no longer be able to skip previously read text.");
-                Logger.LogError(__exception);
-                
+                Logger.Log(LogLevel.Warning | LogLevel.Message, "ADV memory file was corrupted, you might no longer be able to skip previously read text.");
+                Logger.LogWarning(__exception);
+
                 File.Delete(Path.Combine(UserData.Path, "save/read.dat"));
                 // Try again, this time it'll create a blank state since file doesn't exist
                 MainScenario.LoadReadInfo();
@@ -74,12 +77,60 @@ namespace IllusionFixes
         {
             if (__exception != null)
             {
-                Logger.Log(LogLevel.Error | LogLevel.Message, "User ID file was corrupted, you might no longer have access to the cards you uploaded to the uploader.");
-                Logger.LogError(__exception);
+                Logger.Log(LogLevel.Warning | LogLevel.Message, "User ID file was corrupted, you might no longer have access to the cards you uploaded to the uploader.");
+                Logger.LogWarning(__exception);
                 __result = string.Empty;
             }
 
             return null;
+        }
+
+        /// <summary>
+        /// Fixes broken color presets files breaking maker color picker
+        /// </summary>
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(ColorPresets), nameof(ColorPresets.LoadPresets))]
+        [HarmonyPatch(typeof(UI_ColorPresets), nameof(UI_ColorPresets.LoadPresets))]
+        private static void LoadPresetsPrefix(UI_ColorPresets __instance, string ___saveDir)
+        {
+            var path = ___saveDir + "ColorPresets.json";
+
+            if (!File.Exists(path))
+                return;
+
+            try
+            {
+                var json = File.ReadAllText(path);
+                var info = JsonUtility.FromJson<UI_ColorPresets.ColorInfo>(json);
+                if (info == null) throw new Exception("Data is in invalid format");
+            }
+            catch (Exception ex)
+            {
+                File.Delete(path);
+                Logger.Log(LogLevel.Warning | LogLevel.Message, "ColorPresets.json file was corrupted and had to be removed. Saved color presets will be lost.");
+                Logger.LogWarning(ex);
+            }
+        }
+
+        /// <summary>
+        /// Fixes scenes failing to load because of corrupted json color data
+        /// public static object FromJson(string json, Type type)
+        /// </summary>
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(JsonUtility), nameof(JsonUtility.FromJson), typeof(string), typeof(Type))]
+        private static void FromJsonPrefix(ref string json)
+        {
+            var i = json.IndexOf('{');
+            var i2 = json.LastIndexOf('}');
+            if (i > 0 && i2 > 0 && i2 > i)
+            {
+                Logger.LogWarning("Mangled Json data detected, attempting to fix - " + json);
+                json = json.Substring(i, i2 - i + 1);
+            }
+            else if (i < 0 || i2 < 0 || i2 < i)
+            {
+                Logger.LogWarning("Invalid Json data detected");
+            }
         }
     }
 }
