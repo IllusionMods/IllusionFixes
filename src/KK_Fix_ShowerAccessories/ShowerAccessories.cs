5ad6a556d724b9d76ab3a496c4dfb314be7414a7
ManlyMarco <dummyaccx@gmail.com>
2020-07-20 00:15:22 +0200

    Simplify project structure

--- src/KK_Fix_ShowerAccessories/ShowerAccessories.cs
+++ src/KK_Fix_ShowerAccessories/ShowerAccessories.cs
@@ -1,6 +1,12 @@
-﻿using BepInEx;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ActionGame;
+using ActionGame.Chara;
+using BepInEx;
 using BepInEx.Harmony;
 using Common;
+using HarmonyLib;
 
 namespace IllusionFixes
 {
@@ -10,11 +16,93 @@ namespace IllusionFixes
     [BepInProcess(Constants.GameProcessName)]
     [BepInProcess(Constants.GameProcessNameSteam)]
     [BepInPlugin(GUID, PluginName, Constants.PluginsVersion)]
-    public partial class ShowerAccessories : BaseUnityPlugin
+    public class ShowerAccessories : BaseUnityPlugin
     {
         public const string GUID = "KK_Fix_ShowerAccessories";
         public const string PluginName = "Shower Accessories Fix";
 
-        internal void Awake() => HarmonyWrapper.PatchAll(typeof(Hooks));
+        // Disable these when entering shower even if they are set as main accessories
+        private static readonly List<AccessoryEntry> _accessoryBlacklist = new List<AccessoryEntry>
+        {
+            new AccessoryEntry(128, 0, "a_n_shoulder"),
+            new AccessoryEntry(128, 1, "a_n_shoulder"),
+            new AccessoryEntry(129, 0, "a_n_hand"),
+            new AccessoryEntry(125, 0, "a_n_back"),
+            new AccessoryEntry(125, 1, "a_n_back"),
+            new AccessoryEntry(125, 2, "a_n_back"),
+            new AccessoryEntry(125, 12, "a_n_back")
+        };
+
+        private void Awake() => HarmonyWrapper.PatchAll(typeof(Hooks));
+
+        private static void FixAccessoryState(ChaControl chaControl)
+        {
+            //Turn accessories back on and then turn off only Sub accessories
+            chaControl.SetAccessoryStateAll(true);
+            chaControl.SetAccessoryStateCategory(1, false);
+
+            // Turn off backpacks that are set as main accessories by default
+            for (var index = 0; index < chaControl.nowCoordinate.accessory.parts.Length; index++)
+            {
+                var acc = chaControl.nowCoordinate.accessory.parts[index];
+                if (_accessoryBlacklist.Any(x =>
+                    acc.type == x.Type && acc.id == x.Id && acc.parentKey.StartsWith(x.ParentKey)))
+                    chaControl.fileStatus.showAccessory[index] = false;
+            }
+        }
+
+        private readonly struct AccessoryEntry
+        {
+            public readonly int Type;
+            public readonly int Id;
+            public readonly string ParentKey;
+
+            public AccessoryEntry(int type, int id, string parentKey)
+            {
+                Type = type;
+                Id = id;
+                ParentKey = parentKey;
+            }
+        }
+
+        private static class Hooks
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(HSceneProc), "MapSameObjectDisable")]
+            internal static void HSceneShowerFix()
+            {
+                try
+                {
+                    var map = (ActionMap)AccessTools.Field(typeof(HSceneProc), "map").GetValue(Singleton<HSceneProc>.Instance);
+                    if (map.no == 52) //shower
+                    {
+                        var lstFemale = (List<ChaControl>)AccessTools.Field(typeof(HSceneProc), "lstFemale").GetValue(Singleton<HSceneProc>.Instance);
+                        FixAccessoryState(lstFemale[0]);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    UnityEngine.Debug.LogException(ex);
+                }
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(AI), "ArrivalSet")]
+            internal static void OverworldShowerFix(AI __instance, ActionControl.ResultInfo result)
+            {
+                try
+                {
+                    if (result.actionNo == 2) //shower
+                    {
+                        var chaControl = Traverse.Create(__instance).Property("npc").Property<ChaControl>("chaCtrl").Value;
+                        FixAccessoryState(chaControl);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    UnityEngine.Debug.LogException(ex);
+                }
+            }
+        }
     }
 }
