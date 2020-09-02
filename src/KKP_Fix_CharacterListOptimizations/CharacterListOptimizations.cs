4b4e24e3717c1c0ab6f643e31910484428e759bd
ManlyMarco <dummyaccx@gmail.com>
2020-09-03 00:53:22 +0200

    Added CharacterListOptimizations for KK Party and VR modules

---
+++ src/KKP_Fix_CharacterListOptimizations/CharacterListOptimizations.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using BepInEx;
+using BepInEx.Harmony;
+using Common;
+using ExtensibleSaveFormat;
+using HarmonyLib;
+using Localize.Translate;
+
+namespace IllusionFixes
+{
+    [BepInProcess(Constants.GameProcessNameSteam)] //Not currently compatible with Koikatsu Party
+    [BepInProcess(Constants.VRProcessNameSteam)]
+    [BepInPlugin(GUID, PluginName, Constants.PluginsVersion)]
+    public class CharacterListOptimizations : BaseUnityPlugin
+    {
+        public const string GUID = "KK_Fix_CharacterListOptimizations";
+        public const string PluginName = "Character List Optimizations";
+
+        internal void Awake()
+        {
+            if (IncompatiblePluginDetector.AnyIncompatiblePlugins()) return;
+
+            HarmonyWrapper.PatchAll(typeof(Hooks));
+        }
+
+        private class Hooks
+        {
+            /// <summary>
+            /// Turn ExtensibleSaveFormat events off and then back on after the characters get loaded
+            /// </summary>
+            [HarmonyPrefix, HarmonyPatch(typeof(CustomFileListSelecter), "Initialize")]
+            internal static void ListInitPrefix() => ExtendedSave.LoadEventsEnabled = false;
+            [HarmonyPostfix, HarmonyPatch(typeof(CustomFileListSelecter), "Initialize")]
+            internal static void ListInitPostfix() => ExtendedSave.LoadEventsEnabled = true;
+
+            /// <summary>
+            /// After choosing a card reload it to trigger the ExtensibleSaveFormat events
+            /// </summary>
+            [HarmonyPostfix, HarmonyPatch(typeof(CustomFileListSelecter), "Awake")]
+            internal static void ListAwake(CustomFileListSelecter __instance)
+            {
+                __instance.onEnter += control =>
+                {
+                    var files = Traverse.Create(__instance).Field<Localize.Translate.Manager.ChaFileInfo[]>("files").Value;
+                    var fileInfo = files.First(x => x.chaFile == control);
+                    control.LoadCharaFile(fileInfo.info.FullPath);
+                };
+            }
+        }
+    }
+}
--- src/KK_Fix_CharacterListOptimizations/CharacterListOptimizations.cs
+++ src/KK_Fix_CharacterListOptimizations/CharacterListOptimizations.cs
@@ -1,13 +1,25 @@
-﻿using BepInEx;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ActionGame;
+using BepInEx;
 using BepInEx.Harmony;
+using ChaCustom;
 using Common;
+using ExtensibleSaveFormat;
+using FreeH;
+using HarmonyLib;
+using Illusion.Game;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace IllusionFixes
 {
-    [BepInProcess(Constants.GameProcessName)] //Not currently compatible with Koikatsu Party
+    [BepInProcess(Constants.GameProcessName)] // Need a completely separate version for KK Party
     [BepInProcess(Constants.VRProcessName)]
     [BepInPlugin(GUID, PluginName, Constants.PluginsVersion)]
-    public partial class CharacterListOptimizations : BaseUnityPlugin
+    public class CharacterListOptimizations : BaseUnityPlugin
     {
         public const string GUID = "KK_Fix_CharacterListOptimizations";
         public const string PluginName = "Character List Optimizations";
@@ -16,7 +28,93 @@ namespace IllusionFixes
         {
             if (IncompatiblePluginDetector.AnyIncompatiblePlugins()) return;
 
-            HarmonyWrapper.PatchAll(typeof(Hooks));
+            var h = HarmonyWrapper.PatchAll(typeof(Hooks));
+
+            // The VR classes are only available in VR module so they can't be directly referenced
+            var vrType = Type.GetType("VR.VRClassRoomCharaFile, Assembly-CSharp");
+            if (vrType != null)
+            {
+                h.Patch(vrType.GetMethod("InitializeList", AccessTools.all),
+                    prefix: new HarmonyMethod(typeof(Hooks), nameof(Hooks.ClassRoomCharaFileInitializeListPrefix)),
+                    postfix: new HarmonyMethod(typeof(Hooks), nameof(Hooks.ClassRoomCharaFileInitializeListPostfix)));
+                h.Patch(vrType.GetMethod("Start", AccessTools.all),
+                    postfix: new HarmonyMethod(typeof(Hooks), nameof(Hooks.ClassRoomCharaFileStartPostfix)));
+            }
+        }
+
+        internal class Hooks
+        {
+            #region Free H List
+            /// <summary>
+            /// Turn off ExtensibleSaveFormat events
+            /// </summary>
+            [HarmonyPrefix, HarmonyPatch(typeof(FreeHClassRoomCharaFile), "Start")]
+            internal static void FreeHClassRoomCharaFileStartPrefix() => ExtendedSave.LoadEventsEnabled = false;
+            /// <summary>
+            /// Turn back on ExtensibleSaveFormat events, load a copy of the character with extended data on this time, and use that instead.
+            /// </summary>
+            [HarmonyPostfix, HarmonyPatch(typeof(FreeHClassRoomCharaFile), "Start")]
+            internal static void FreeHClassRoomCharaFileStartPostfix(FreeHClassRoomCharaFile __instance)
+            {
+                ExtendedSave.LoadEventsEnabled = true;
+
+                ReactiveProperty<ChaFileControl> info = Traverse.Create(__instance).Field("info").GetValue<ReactiveProperty<ChaFileControl>>();
+                ClassRoomFileListCtrl listCtrl = Traverse.Create(__instance).Field("listCtrl").GetValue<ClassRoomFileListCtrl>();
+                List<CustomFileInfo> lstFileInfo = Traverse.Create(listCtrl).Field("lstFileInfo").GetValue<List<CustomFileInfo>>();
+                Button enterButton = Traverse.Create(__instance).Field("enterButton").GetValue<Button>();
+
+                enterButton.onClick.RemoveAllListeners();
+                enterButton.onClick.AddListener(() =>
+                {
+                    var onEnter = (Action<ChaFileControl>)AccessTools.Field(typeof(FreeHClassRoomCharaFile), "onEnter").GetValue(__instance);
+                    string fullPath = lstFileInfo.First(x => x.FileName == info.Value.charaFileName.Remove(info.Value.charaFileName.Length - 4)).FullPath;
+
+                    ChaFileControl chaFileControl = new ChaFileControl();
+                    chaFileControl.LoadCharaFile(fullPath, info.Value.parameter.sex, false, true);
+
+                    onEnter(chaFileControl);
+                    Utils.Sound.Play(SystemSE.sel);
+                });
+            }
+            #endregion
+
+            #region Classroom list / VR Free H list
+            /// <summary>
+            /// Turn off ExtensibleSaveFormat events
+            /// </summary>
+            [HarmonyPrefix, HarmonyPatch(typeof(ClassRoomCharaFile), "InitializeList")]
+            internal static void ClassRoomCharaFileInitializeListPrefix() => ExtendedSave.LoadEventsEnabled = false;
+            /// <summary>
+            /// Turn back on ExtensibleSaveFormat events
+            /// </summary>
+            [HarmonyPostfix, HarmonyPatch(typeof(ClassRoomCharaFile), "InitializeList")]
+            internal static void ClassRoomCharaFileInitializeListPostfix() => ExtendedSave.LoadEventsEnabled = true;
+            /// <summary>
+            /// Load a copy of the character with extended data on this time, and use that instead.
+            /// Need to keep the object types vague since this is also used for VR list which is of different type
+            /// </summary>
+            [HarmonyPostfix, HarmonyPatch(typeof(ClassRoomCharaFile), "Start")]
+            internal static void ClassRoomCharaFileStartPostfix(object __instance)
+            {
+                var traverse = Traverse.Create(__instance);
+                var info = traverse.Field("info").GetValue<ReactiveProperty<ChaFileControl>>();
+                var lstFileInfo = traverse.Field("listCtrl").Field("lstFileInfo").GetValue<List<CustomFileInfo>>();
+                var enterButton = traverse.Field("enterButton").GetValue<Button>();
+
+                enterButton.onClick.RemoveAllListeners();
+                enterButton.onClick.AddListener(() =>
+                {
+                    var onEnter = traverse.Field("onEnter").GetValue<Action<ChaFileControl>>();
+                    string fullPath = lstFileInfo.First(x => x.FileName == info.Value.charaFileName.Remove(info.Value.charaFileName.Length - 4)).FullPath;
+
+                    ChaFileControl chaFileControl = new ChaFileControl();
+                    chaFileControl.LoadCharaFile(fullPath, info.Value.parameter.sex, false, true);
+
+                    onEnter(chaFileControl);
+                    Utils.Sound.Play(SystemSE.sel);
+                });
+            }
+            #endregion
         }
     }
 }
