8f85604c5496f64664f3afc82f7d825b7a12454a
ManlyMarco <dummyaccx@gmail.com>
2020-09-13 20:27:09 +0200

    Version up to v14.2

--- src/Common/Constants.cs
+++ src/Common/Constants.cs
@@ -6,7 +6,7 @@
         /// Version number used by all plugins. Must be convertible to <see cref="System.Version"/>
         /// Uses ReleaseVersion.HotfixVersion format.
         /// </summary>
-        public const string PluginsVersion = "14.1";
+        public const string PluginsVersion = "14.2";
 
 #if AI
         internal const string StudioProcessName = "StudioNEOV2";
