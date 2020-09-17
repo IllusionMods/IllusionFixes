874f9df7866af82beaf29600bafeed4c5a167984
ManlyMarco <dummyaccx@gmail.com>
2020-09-18 00:24:06 +0200

    Move garbage alloc hooks to a new project

---
+++ src/KK_Fix_GarbageTruck/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle(IllusionFixes.GarbageTruck.GUID)]
+[assembly: AssemblyProduct(IllusionFixes.GarbageTruck.GUID)]
+[assembly: AssemblyDescription(IllusionFixes.GarbageTruck.GUID)]
