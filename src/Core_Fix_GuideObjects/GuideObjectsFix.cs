a46a3e8c4b23b60d93f27ce93e48898cca8145ae
ManlyMarco <dummyaccx@gmail.com>
2020-10-13 13:04:52 +0200

    Fix acc guide objects not having speed and scale sliders applied at start; Disable draw on top hack for rotation guide object

--- src/Core_Fix_GuideObjects/GuideObjectsFix.cs
+++ src/Core_Fix_GuideObjects/GuideObjectsFix.cs
@@ -15,9 +15,28 @@ namespace IllusionFixes
 
         private static class Hooks
         {
+            /// <summary>
+            /// Fix accessory guide objects not having speed and scale sliders applied right after first enabling them
+            /// </summary>
+            [HarmonyPrefix, HarmonyPatch(typeof(CustomUtility.CustomGuideObject), "Awake")]
+            internal static void CustomGuideObject_Awake(CustomUtility.CustomGuideObject __instance)
+            {
+                var id = __instance.name.EndsWith("1") ? 0 : 1;
+                var css = Singleton<ChaCustom.CustomBase>.Instance.customSettingSave;
+                __instance.scaleAxis = css.controllerScale[id];
+                __instance.speedMove = css.controllerSpeed[id];
+            }
+
+            /// <summary>
+            /// Fix accessory guide objects not being on top of everything like studio guide objects
+            /// </summary>
             [HarmonyPostfix, HarmonyPatch(typeof(CustomUtility.CustomGuideBase), "Start")]
             internal static void CustomGuideBase_Start(CustomUtility.CustomGuideBase __instance, ref Color ___colorNormal, ref Color ___colorHighlighted)
             {
+                // Rotation guides have serious issues with z fighting
+                if (__instance.transform.parent.name == "rotation")
+                    return;
+
                 if (__instance.gameObject.name == "X" || __instance.gameObject.name == "Y" || __instance.gameObject.name == "Z")
                 {
                     ___colorNormal.a = 1f;
@@ -43,17 +62,6 @@ namespace IllusionFixes
                 __instance.material.SetInt("_ZWrite", 0);
                 __instance.material.SetInt("_ZTest", 0);
                 __instance.material.color = ___colorNormal;
-
-                if (__instance.transform.parent.name == "rotation")
-                {
-                    //Prevent z fighting
-                    if (__instance.gameObject.name == "X")
-                        __instance.material.renderQueue = __instance.material.renderQueue + 1;
-                    else if (__instance.gameObject.name == "Y")
-                        __instance.material.renderQueue = __instance.material.renderQueue + 2;
-                    else if (__instance.gameObject.name == "Z")
-                        __instance.material.renderQueue = __instance.material.renderQueue + 3;
-                }
             }
         }
     }
