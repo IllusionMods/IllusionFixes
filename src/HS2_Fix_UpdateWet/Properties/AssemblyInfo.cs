9bf7398f37c451cda970749d3a687ddb3d88f767
OrangeSpork <57274555+OrangeSpork@users.noreply.github.com>
2021-02-26 16:32:16 -0700

    Fix the ChaControl.UpdateWet NullReferenceException

---
+++ src/HS2_Fix_UpdateWet/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle(IllusionFixes.FixUpdateWet.GUID)]
+[assembly: AssemblyProduct(IllusionFixes.FixUpdateWet.GUID)]
+[assembly: AssemblyDescription(IllusionFixes.FixUpdateWet.PluginName)]
