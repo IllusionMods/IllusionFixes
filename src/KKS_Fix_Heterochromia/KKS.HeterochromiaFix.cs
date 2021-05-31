b4d9f275bba51a695859e5715afc3d9dbce23907
ManlyMarco <dummyaccx@gmail.com>
2021-05-31 20:30:47 +0200

    Port KKS_Fix_Heterochromia

---
+++ src/KKS_Fix_Heterochromia/KKS.HeterochromiaFix.cs
@@ -0,0 +1,12 @@
+﻿using BepInEx;
+using Common;
+
+namespace IllusionFixes
+{
+    [BepInProcess(Constants.GameProcessName)]
+    [BepInPlugin(GUID, PluginName, Constants.PluginsVersion)]
+    public partial class HeterochromiaFix : BaseUnityPlugin
+    {
+        public const string GUID = "KKS_Fix_Heterochromia";
+    }
+}
