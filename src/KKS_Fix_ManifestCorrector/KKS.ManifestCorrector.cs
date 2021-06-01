0dccc2ea5b30d0fd844cc0f7a70c330228987ab9
DeathWeasel1337 <deathweasel1337@gmail.com>
2021-06-01 12:55:24 -0500

    KKS port

---
+++ src/KKS_Fix_ManifestCorrector/KKS.ManifestCorrector.cs
@@ -0,0 +1,12 @@
+﻿using BepInEx;
+using Common;
+
+namespace IllusionFixes
+{
+    [BepInProcess(Constants.GameProcessName)]
+    [BepInPlugin(GUID, PluginName, Constants.PluginsVersion)]
+    public partial class ManifestCorrector : BaseUnityPlugin
+    {
+        public const string GUID = "KKS_Fix_ManifestCorrector";
+    }
+}
