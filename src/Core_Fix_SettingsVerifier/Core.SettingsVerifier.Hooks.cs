ea9ecd5da8ffbf49bcd82d81f6438bec60ff3fa6
DeathWeasel1337 <deathweasel1337@gmail.com>
2021-06-01 12:32:19 -0500

    KKS port

--- src/Core_Fix_SettingsVerifier/Core.SettingsVerifier.Hooks.cs
+++ src/Core_Fix_SettingsVerifier/Core.SettingsVerifier.Hooks.cs
@@ -15,10 +15,10 @@ namespace IllusionFixes
             /// <summary>
             /// Run the code for reading setup.xml when inside studio. Done in a Manager.Config.Start hook because the xmlRead method needs stuff to be initialized first.
             /// </summary>
-#if HS2
+#if HS2 || KKS
             [HarmonyPostfix, HarmonyPatch(typeof(Manager.Config), MethodType.StaticConstructor)]
 #else
-            [HarmonyPostfix, HarmonyPatch(typeof(Manager.Config), "Start")]
+            [HarmonyPostfix, HarmonyPatch(typeof(Manager.Config), nameof(Manager.Config.Start))]
 #endif
             internal static void ManagerConfigStart()
             {
---
+++ src/KKS_Fix_SettingsVerifier/KKS.SettingsVerifier.cs
@@ -0,0 +1,12 @@
+﻿using BepInEx;
+using Common;
+
+namespace IllusionFixes
+{
+    [BepInProcess(Constants.GameProcessName)]
+    [BepInPlugin(GUID, PluginName, Constants.PluginsVersion)]
+    public partial class SettingsVerifier : BaseUnityPlugin
+    {
+        public const string GUID = "KKS_Fix_SettingsVerifier";
+    }
+}
