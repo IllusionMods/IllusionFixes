c604c608f8e8c5e9019f6eeb2d41e8760943ae16
OrangeSpork <57274555+OrangeSpork@users.noreply.github.com>
2021-07-08 04:33:59 -0600

    Male face type fix (#27)
    Illusion hides the face type selections for males in chara creator (as they don't ship any). We've got modders looking at adding more male face types...this just pops the apparently perfectly functional UI back to active state.

---
+++ src/Core_Fix_MakerMaleFaceTypes/Core.MakerMaleFaceTypes.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using CharaCustom;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace IllusionFixes
+{
+    public partial class MakerMaleFaceTypes
+    {
+        public const string PluginName = "Maker Male Face Types";
+
+        internal static new ManualLogSource Logger;
+
+        private static MakerMaleFaceTypes Instance;
+
+        public MakerMaleFaceTypes()
+        {
+            Instance = this;
+            Logger = base.Logger;
+        }
+
+        private void Awake()
+        {
+            KKAPI.Maker.MakerAPI.MakerFinishedLoading += MakerAPI_MakerFinishedLoading;
+        }
+    }
+}
---
+++ src/HS2_Fix_MakerMaleFaceTypes/HS2.MakerMaleFaceTypes.cs
@@ -0,0 +1,29 @@
+﻿using AIChara;
+using BepInEx;
+using CharaCustom;
+using Common;
+using KKAPI.Maker;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IllusionFixes
+{
+    [BepInProcess(Constants.GameProcessName)]
+    [BepInPlugin(GUID, PluginName, Constants.PluginsVersion)]
+    public partial class MakerMaleFaceTypes : BaseUnityPlugin
+    {
+        public const string GUID = "HS2_Fix_MakerMaleFaceTypes";
+
+        private void MakerAPI_MakerFinishedLoading(object sender, EventArgs e)
+        {
+            if (MakerAPI.GetMakerSex() == 0)
+            {
+                CvsF_FaceType faceType = GameObject.Find("CharaCustom").transform.GetComponentInChildren<CvsF_FaceType>();
+                faceType.transform.Find("Setting/Setting01/title").gameObject.SetActive(true);
+                faceType.transform.Find("Setting/Setting01/SelectBox").gameObject.SetActive(true);
+                faceType.transform.Find("Setting/Setting01/separate").gameObject.SetActive(true);
+            }
+        }
+    }
+}
