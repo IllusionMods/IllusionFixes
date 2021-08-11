16c1c8e49199dcfe32be2e6add1819b3b021e1bf
ManlyMarco <dummyaccx@gmail.com>
2021-08-11 21:46:04 +0200

    Added Fix_WetAccessoriesFix for AI and HS2

---
+++ src/Core_Fix_WetAccessories/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using IllusionFixes;
+
+[assembly: AssemblyTitle(WetAccessoriesFix.GUID)]
+[assembly: AssemblyProduct(WetAccessoriesFix.GUID)]
+[assembly: AssemblyDescription(WetAccessoriesFix.PluginName)]
---
+++ src/Core_Fix_WetAccessories/WetAccessoriesFix.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using AIChara;
+using BepInEx;
+using Common;
+using HarmonyLib;
+using UnityEngine;
+
+namespace IllusionFixes
+{
+    [BepInPlugin(GUID, PluginName, Constants.PluginsVersion)]
+    public partial class WetAccessoriesFix : BaseUnityPlugin
+    {
+        public const string GUID = "Fix_WetAccessoriesFix";
+        public const string PluginName = "Wet Accessories Fix";
+
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(WetAccessoriesFix));
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(ChaControl), nameof(ChaControl.UpdateWet))]
+        private static void FixWetAccs(ChaControl __instance)
+        {
+            try
+            {
+                var wetRate = __instance.fileStatus.wetRate;
+
+                foreach (var accessory in __instance.cmpAccessory)
+                {
+                    if (accessory == null) continue;
+                    ApplyWetRate(accessory.rendNormal, wetRate);
+                    ApplyWetRate(accessory.rendAlpha, wetRate);
+                }
+            }
+            catch (Exception ex)
+            {
+                UnityEngine.Debug.LogException(ex);
+            }
+        }
+
+        private static void ApplyWetRate(Renderer[] renderers, float wetRate)
+        {
+            if (renderers == null) return;
+            foreach (var renderer in renderers)
+            {
+                if (renderer == null) continue;
+                foreach (var material in renderer.materials)
+                {
+                    if (material == null) continue;
+                    if (material.HasProperty(ChaShader.wetRate))
+                    {
+                        material.SetFloat(ChaShader.wetRate, wetRate);
+                    }
+                }
+            }
+        }
+    }
+}
