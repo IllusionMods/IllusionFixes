79a8129816f7ee9395362b5f6ae20d0e15285739
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2022-03-23 01:07:02 +0100

    [KK/KKS][DynamicBonesFix] Fix DynamicBone exclusions list not working (crashing) Thanks to essu for finding the issue and making a POC fix, and Wogrim for testing

--- src/Core_Fix_DynamicBones/Core.DynamicBonesFix.cs
+++ src/Core_Fix_DynamicBones/Core.DynamicBonesFix.cs
@@ -1,6 +1,10 @@
-﻿using BepInEx;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using BepInEx;
 using Common;
 using HarmonyLib;
+using UnityEngine;
 
 namespace IllusionFixes
 {
@@ -20,6 +24,37 @@ namespace IllusionFixes
             internal static bool SkipUpdateParticles() => false;
             [HarmonyPrefix, HarmonyPatch(typeof(DynamicBone_Ver02), nameof(DynamicBone_Ver02.SkipUpdateParticles))]
             internal static bool SkipUpdateParticlesVer02() => false;
+
+            //Fix dynamicbone exclusions feature not working (crashing)
+            [HarmonyTranspiler, HarmonyPatch(typeof(DynamicBone), nameof(DynamicBone.AppendParticles))]
+            internal static IEnumerable<CodeInstruction> FixAppendParticlesExclusionsCrash(IEnumerable<CodeInstruction> instructions)
+            {
+                // The issue is that the loop increments the wrong index variable (the variable for the outer loop)
+                // To fix it: find the loop, find reference of its index variable, replace references to the wrong variable
+                var cm = new CodeMatcher(instructions);
+                cm.MatchForward(true,
+                        new CodeMatch(OpCodes.Ldarg_0),
+                        new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(DynamicBone), nameof(DynamicBone.m_Exclusions))),
+                        new CodeMatch(OpCodes.Brfalse),
+                        new CodeMatch(OpCodes.Ldc_I4_0),
+                        new CodeMatch(OpCodes.Stloc_S))
+                    .ThrowIfNotMatch("1", new CodeMatch(OpCodes.Stloc_S), new CodeMatch(OpCodes.Br));
+                // Reference to the loop's index variable
+                var loopLocal = cm.Operand;
+                // Find and replace the offending references to the outer index variable (this is a ++ increment operation)
+                cm.MatchForward(false,
+                        new CodeMatch(OpCodes.Ldloc_S),
+                        new CodeMatch(OpCodes.Ldc_I4_1),
+                        new CodeMatch(OpCodes.Add),
+                        new CodeMatch(OpCodes.Stloc_S))
+                    .ThrowIfFalse("2", matcher => matcher.Opcode == OpCodes.Ldloc_S && matcher.Operand != loopLocal)
+                    .SetOperandAndAdvance(loopLocal)
+                    .Advance(2)
+                    .ThrowIfFalse("3", matcher => matcher.Opcode == OpCodes.Stloc_S && matcher.Operand != loopLocal)
+                    .SetOperandAndAdvance(loopLocal);
+                // yay, thanks to essu for finding the issue
+                return cm.Instructions();
+            }
         }
     }
 }
