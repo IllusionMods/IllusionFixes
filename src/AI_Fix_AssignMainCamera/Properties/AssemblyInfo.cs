8f5736288f54d31c446f5144c80745ed5634cbf5
KKY <8879012+kkykkykky@users.noreply.github.com>
2022-07-20 12:51:59 -0600

    Add AI_Fix_AssignMainCamera
    Set CameraControl.mainCmaera to Camera.main in AI for consistency across games.

---
+++ src/AI_Fix_AssignMainCamera/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle(IllusionFixes.AssignMainCamera.GUID)]
+[assembly: AssemblyProduct(IllusionFixes.AssignMainCamera.GUID)]
+[assembly: AssemblyDescription(IllusionFixes.AssignMainCamera.PluginName)]
