51e1087061d38e6fa95d0f7c4c97312945b1daae
KKY <8879012+kkykkykky@users.noreply.github.com>
2022-07-22 00:21:38 -0600

    Add AI_Fix_AssignMainCamera (#41)
    Set CameraControl.mainCmaera to Camera.main in AI for consistency across games.

---
+++ src/AI_Fix_AssignMainCamera/AssignMainCamera.cs
@@ -0,0 +1,29 @@
+﻿using Studio;
+using BepInEx;
+using HarmonyLib;
+using Common;
+using UnityEngine;
+
+namespace IllusionFixes
+{
+    [BepInProcess(Constants.StudioProcessName)]
+    [BepInPlugin(GUID, PluginName, Constants.PluginsVersion)]
+    public class AssignMainCamera : BaseUnityPlugin
+    {
+        public const string GUID = "kky.ai.assignmaincamera";
+        public const string PluginName = "AI Assign Main Camera Fix";
+
+        private void Awake()
+        {
+            var harmony = new Harmony(nameof(AssignMainCamera));
+            harmony.PatchAll(typeof(AssignMainCamera));
+        }
+
+        [HarmonyPatch(typeof(Studio.CameraControl), "Awake")]
+        [HarmonyPostfix]
+        public static void setMainCamera(Studio.CameraControl __instance)
+        {
+            if (__instance.mainCmaera == null) __instance.mainCmaera = Camera.main;
+        }
+    }
+}
