f292d846086d3f85e258dd4650df4e46b8b02f0d
takahiro kawanaka <takahiro0327@hotmail.com>
2024-02-17 19:39:17 +0900

    Remove unused variables

--- src/Core_Fix_StudioOptimizations/StudioOptimizations.cs
+++ src/Core_Fix_StudioOptimizations/StudioOptimizations.cs
@@ -38,14 +38,6 @@ namespace IllusionFixes
 #else
         public static HashSet<string> AccessoryAttachPoints = new HashSet<string> { "a_n_nip_L", "a_n_nip_R", "a_n_shoulder_L", "a_n_arm_L", "a_n_wrist_L", "a_n_hand_L", "a_n_ind_L", "a_n_mid_L", "a_n_ring_L", "a_n_elbo_L", "a_n_shoulder_R", "a_n_arm_R", "a_n_wrist_R", "a_n_hand_R", "a_n_ind_R", "a_n_mid_R", "a_n_ring_R", "a_n_elbo_R", "a_n_mouth", "a_n_hair_pin_R", "a_n_hair_pin", "a_n_hair_pony", "a_n_hair_twin_L", "a_n_hair_twin_R", "a_n_head", "a_n_headflont", "a_n_headside", "a_n_headtop", "a_n_earrings_L", "a_n_earrings_R", "a_n_nose", "a_n_megane", "a_n_neck", "a_n_back", "a_n_back_L", "a_n_back_R", "a_n_bust", "a_n_bust_f", "a_n_ana", "a_n_kokan", "a_n_dan", "a_n_leg_L", "a_n_ankle_L", "a_n_heel_L", "a_n_knee_L", "a_n_leg_R", "a_n_ankle_R", "a_n_heel_R", "a_n_knee_R", "a_n_waist", "a_n_waist_b", "a_n_waist_f", "a_n_waist_L", "a_n_waist_R" };
 #endif
-        /// <summary>
-        /// Map from the name of transform to path where it resides
-        /// </summary>
-        private static Dictionary<string, List<string>> NameToPathMap = new Dictionary<string, List<string>>();
-
-        //Variables to avoid GC
-        private static List<string> _transformPaths = new List<string>();
-        private static StringBuilder _pathBuilder = new StringBuilder();
 
         private static FindLoopAssistant assistant = new FindLoopAssistant(AccessoryAttachPoints);
 
