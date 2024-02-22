86d088a8c995fccf3e7c63632022d88d83d9a36e
takahiro kawanaka <takahiro0327@hotmail.com>
2024-02-23 01:52:52 +0900

    Fixed a bug where the initial rotation amount kept being applied to IK

---
+++ src/Core_Fix_StudioOptimizations/FixGuideObject.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Studio;
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace IllusionFixes
+{
+    public partial class StudioOptimizations : BaseUnityPlugin
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Studio.GuideObject), nameof(Studio.GuideObject.CalcRotation))]
+        private static void GuideObjectCalcRotationPrefix(Studio.GuideObject __instance)
+        {
+            if( !__instance.m_Enables[1] && __instance.transformTarget != null && __instance.mode == GuideObject.Mode.LocalIK )
+            {
+                //Initialize rotation so that IKs with invalid rotation follow the parent's rotation
+                __instance.transformTarget.localRotation = Quaternion.identity;
+            }
+        }
+    }
+}
