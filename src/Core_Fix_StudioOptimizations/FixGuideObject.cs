13c364d3e69083ae4f8ec309db6ca719c23ba11e
takahiro kawanaka <takahiro0327@hotmail.com>
2024-02-23 23:12:34 +0900

    little faster.

--- src/Core_Fix_StudioOptimizations/FixGuideObject.cs
+++ src/Core_Fix_StudioOptimizations/FixGuideObject.cs
@@ -11,7 +11,7 @@ namespace IllusionFixes
         [HarmonyPatch(typeof(Studio.GuideObject), nameof(Studio.GuideObject.CalcRotation))]
         private static void GuideObjectCalcRotationPrefix(Studio.GuideObject __instance)
         {
-            if( !__instance.m_Enables[1] && __instance.transformTarget != null && __instance.mode == GuideObject.Mode.LocalIK )
+            if( __instance.mode == GuideObject.Mode.LocalIK && !__instance.m_Enables[1] && __instance.transformTarget != null )
             {
                 //Initialize rotation so that IKs with disabled rotation follow the parent's rotation
                 __instance.transformTarget.localRotation = Quaternion.identity;
