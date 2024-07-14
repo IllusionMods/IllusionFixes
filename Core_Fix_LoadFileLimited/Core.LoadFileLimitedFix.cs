66dcaf48246c5de9709579bbdbdd992dcb7364f6
Njaecha <andre.grabowich@gmail.com>
2024-07-14 21:40:10 +0200

    LoadTimeLimitedFix
    Makes LoadFileLimited overwrite the charaFileName if the character parameters were loaded

---
+++ Core_Fix_LoadFileLimited/Core.LoadFileLimitedFix.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Common;
+
+namespace Core_Fix_LoadFileLimited
+{
+    [BepInPlugin(GUID, PluginName, Constants.PluginsVersion)]
+    public partial class LoadFileLimitedFix : BaseUnityPlugin
+    {
+        public const string GUID = "LoadFileLmitedFix";
+        public const string PluginName = "LoadFileLimitedFix";
+
+        internal void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(Hooks));
+        }
+    }
+}
