7f5e9f7e8580670d70afe698eec984dc91058b45
Njaecha <60891851+Njaecha@users.noreply.github.com>
2024-07-14 22:30:19 +0200

    LoadFileLimitedFix (#68)
    Makes LoadFileLimited overwrite the charaFileName if new character parameters were loaded.
    This is useful if plugins what to know what character is currently loaded. Players are most likely to think of a character as currently loaded depending on the parameters (name, birthday, etc), so the fix will not change the charaFileName unless those parameters were replaced when loading a character in maker.

---
+++ Core_Fix_LoadFileLimited/Core.LoadFileLimitedFix.cs
@@ -0,0 +1,50 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Common;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Core_Fix_LoadFileLimited
+{
+    [BepInPlugin(GUID, PluginName, Constants.PluginsVersion)]
+    public class LoadFileLimitedFix : BaseUnityPlugin
+    {
+        public const string GUID = "LoadFileLmitedFix";
+        public const string PluginName = "LoadFileLimitedFix";
+
+        internal void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(Hooks));
+        }
+
+        internal class Hooks
+        {
+            // Makes ChaFileControl.LoadFileLimited (used by Maker to partially load a character) overwrite the charaFileName property whenever new parameters are loaded.
+            // This should keep the value of that property and the character the player thinks of as currently loaded in sync. 
+            [HarmonyTranspiler]
+            [HarmonyPatch(typeof(ChaFileControl), nameof(ChaFileControl.LoadFileLimited), typeof(string), typeof(byte), typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(bool)
+#if KKS
+                    ,typeof(bool)
+#endif
+                )]
+            private static IEnumerable<CodeInstruction> MakeLoadLimitedCopyCardFileNameTooWhenCopyingParameters(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+            {
+                List<CodeInstruction> code = new List<CodeInstruction>(instructions);
+
+                CodeMatcher cm = new CodeMatcher(instructions, generator);
+
+                // match [this.parameter.Copy(chaFileControl.parameter);]
+                cm.MatchForward(false, new CodeMatch(OpCodes.Callvirt, AccessTools.Method(typeof(ChaFileParameter), nameof(ChaFileParameter.Copy))));
+                // insert [base.charaFileName = chaFileControl.charaFileName;]
+                cm.InsertAndAdvance(
+                    new CodeInstruction(OpCodes.Ldarg_0),
+                    new CodeInstruction(OpCodes.Ldloc_1),
+                    new CodeInstruction(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(ChaFile), nameof(ChaFile.charaFileName))),
+                    new CodeInstruction(OpCodes.Call, AccessTools.PropertySetter(typeof(ChaFile), nameof(ChaFile.charaFileName)))
+                    );
+
+                return cm.Instructions();
+            }
+        }
+    }
+}
