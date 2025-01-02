c72d7e0100e1f4be72a1ddd042e9e92a4fd0dedb
Njaecha <60891851+Njaecha@users.noreply.github.com>
2025-01-02 13:29:03 +0100

    DynamicBone AppendParticles rewrite (#72)
    Rewrote the method since it had a logic flaw that made it impossible to have multiple consecutive bones in the m_notRolls list.
    Adding all children to m_Exclusions will still make the chain end prematurely.

--- src/Core_Fix_DynamicBones/Core.DynamicBonesFix.cs
+++ src/Core_Fix_DynamicBones/Core.DynamicBonesFix.cs
@@ -3,6 +3,7 @@ using System.Reflection.Emit;
 using BepInEx;
 using Common;
 using HarmonyLib;
+using UnityEngine;
 
 namespace IllusionFixes
 {
@@ -19,42 +20,110 @@ namespace IllusionFixes
             //Disable the SkipUpdateParticles method since it causes problems, namely causing jittering when the FPS is higher than 60
             [HarmonyPrefix, HarmonyPatch(typeof(DynamicBone), nameof(DynamicBone.SkipUpdateParticles))]
             internal static bool SkipUpdateParticles() => false;
+
             [HarmonyPrefix, HarmonyPatch(typeof(DynamicBone_Ver02), nameof(DynamicBone_Ver02.SkipUpdateParticles))]
             internal static bool SkipUpdateParticlesVer02() => false;
 
-#if KK || EC || KKS
-            // Fix dynamicbone exclusions feature not working (crashing)
-            // This fix is already included in AI and HS2 codebase, but not in KK, EC and KKS
-            [HarmonyTranspiler, HarmonyPatch(typeof(DynamicBone), nameof(DynamicBone.AppendParticles))]
-            internal static IEnumerable<CodeInstruction> FixAppendParticlesExclusionsCrash(IEnumerable<CodeInstruction> instructions)
+            /// <summary>
+            /// Without this it is not possible to add multiple consecutive "chain links" (transforms that would become particles) in the m_notRolls list because the recursive method call loop would end. The m_notRolls list is used to disable/skip chain links in the dynamic bone chain.
+            /// Furthermore this fixes, that, when the chain is ended prematurely (by adding all children of a chain link to the m_Exclusions list), the EndOffset settings will be ignored. It also fixes that, in HS2, the chain would end as soon as m_notRolls was used and an EndOffset is configured.
+            /// The previous fix for the m_Excludes list is included here by using .Contains().
+            /// </summary>
+            public static void AppendParticles(Transform bone, int parentIndex, float boneLength, DynamicBone dynamicBone)
             {
-                // The issue is that the loop increments the wrong index variable (the variable for the outer loop)
-                // To fix it: find the loop, find reference of its index variable, replace references to the wrong variable
-                var cm = new CodeMatcher(instructions);
-                cm.MatchForward(true,
-                                new CodeMatch(OpCodes.Ldarg_0),
-                                new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(DynamicBone), nameof(DynamicBone.m_Exclusions))),
-                                new CodeMatch(OpCodes.Brfalse),
-                                new CodeMatch(OpCodes.Ldc_I4_0),
-                                new CodeMatch(OpCodes.Stloc_S))
-                  .ThrowIfNotMatch("1", new CodeMatch(OpCodes.Stloc_S), new CodeMatch(OpCodes.Br));
-                // Reference to the loop's index variable
-                var loopLocal = cm.Operand;
-                // Find and replace the offending references to the outer index variable (this is a ++ increment operation)
-                cm.MatchForward(false,
-                                new CodeMatch(OpCodes.Ldloc_S),
-                                new CodeMatch(OpCodes.Ldc_I4_1),
-                                new CodeMatch(OpCodes.Add),
-                                new CodeMatch(OpCodes.Stloc_S))
-                  .ThrowIfFalse("2", matcher => matcher.Opcode == OpCodes.Ldloc_S && matcher.Operand != loopLocal)
-                  .SetOperandAndAdvance(loopLocal)
-                  .Advance(2)
-                  .ThrowIfFalse("3", matcher => matcher.Opcode == OpCodes.Stloc_S && matcher.Operand != loopLocal)
-                  .SetOperandAndAdvance(loopLocal);
-                // yay, thanks to essu for finding the issue
-                return cm.Instructions();
+                DynamicBone.Particle particle = new DynamicBone.Particle
+                {
+                    m_Transform = bone,
+                    m_ParentIndex = parentIndex
+                };
+                if (bone)
+                {
+                    particle.m_Position = particle.m_PrevPosition = bone.position;
+                    particle.m_InitLocalPosition = bone.localPosition;
+                    particle.m_InitLocalRotation = bone.localRotation;
+                }
+                else
+                {
+                    Transform parentTransform = dynamicBone.m_Particles[parentIndex].m_Transform;
+                    if (dynamicBone.m_EndLength > 0f)
+                    {
+                        Transform parent = parentTransform.parent;
+                        if (parent)
+                        {
+                            particle.m_EndOffset =
+                                parentTransform.InverseTransformPoint(parentTransform.position * 2f - parent.position) *
+                                dynamicBone.m_EndLength;
+                        }
+                        else
+                        {
+                            particle.m_EndOffset = new Vector3(dynamicBone.m_EndLength, 0f, 0f);
+                        }
+                    }
+                    else
+                    {
+                        particle.m_EndOffset = parentTransform.InverseTransformPoint(
+                            dynamicBone.transform.TransformDirection(dynamicBone.m_EndOffset) +
+                            parentTransform.position);
+                    }
+
+                    particle.m_Position =
+                        (particle.m_PrevPosition = parentTransform.TransformPoint(particle.m_EndOffset));
+                }
+
+                if (parentIndex >= 0)
+                {
+                    boneLength += (dynamicBone.m_Particles[parentIndex].m_Transform.position - particle.m_Position)
+                        .magnitude;
+                    particle.m_BoneLength = boneLength;
+                    dynamicBone.m_BoneTotalLength = Mathf.Max(dynamicBone.m_BoneTotalLength, boneLength);
+                }
+
+                int count = dynamicBone.m_Particles.Count;
+                dynamicBone.m_Particles.Add(particle);
+                while (bone.childCount > 0)
+                {
+                    var isNotRoll = false;
+                    var index = 0;
+                    for (var i = 0; i < bone.childCount; i++)
+                    {
+                        Transform child = bone.GetChild(i);
+                        var notValid = false;
+                        if (dynamicBone.m_Exclusions != null)
+                        {
+                            notValid = dynamicBone.m_Exclusions.Contains(child);
+                        }
+
+                        if (!notValid && dynamicBone.m_notRolls != null)
+                        {
+                            isNotRoll = notValid = dynamicBone.m_notRolls.Contains(child);
+                            index = i;
+                        }
+
+                        // if child is valid particle, append it as new particle
+                        if (!notValid) AppendParticles(child, count, boneLength, dynamicBone);
+                    }
+
+                    // we only end up here if all children were in m_Excludes or at least one child was in m_notRolls
+                    if (isNotRoll) bone = bone.GetChild(index);
+                    else break;
+                }
+                
+                // we only end up here if we have broken out of the loop:
+                // A) because the bone doesn't have any children or B) m_Excludes contains all children
+                if (dynamicBone.m_EndLength > 0f || dynamicBone.m_EndOffset != Vector3.zero)
+                {
+                    // add the final "leaf" particle, based on the EndOffset
+                    AppendParticles(null, count, boneLength, dynamicBone);
+                }
             }
-#endif
+            
+            // divert the call to AppendParticles with a call to the rewritten AppendParticles method above
+            [HarmonyPrefix, HarmonyPatch(typeof(DynamicBone), nameof(DynamicBone.AppendParticles))]
+            internal static bool DivertAppendParticles(DynamicBone __instance, Transform b, int parentIndex, float boneLength)
+            {
+                AppendParticles(b, parentIndex, boneLength, __instance);
+                return false;
+            }
         }
     }
 }
