50858ca7d751f79b8ad8eadcfa61fb39d0866525
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-11-02 00:47:18 +0100

    Version up to 22.1

--- src/Common/Constants.cs
+++ src/Common/Constants.cs
@@ -8,7 +8,7 @@ namespace Common
         /// Version number used by all plugins. Must be convertible to <see cref="System.Version"/>
         /// Uses ReleaseVersion.HotfixVersion format.
         /// </summary>
-        public const string PluginsVersion = "22.0.4";
+        public const string PluginsVersion = "22.1";
 
 #if AI
         internal static bool InsideStudio => Application.productName == StudioProcessName;
